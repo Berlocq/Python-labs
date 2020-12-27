70cc4adaf0a5a86e753d848c83b39375572072c9
Berlocq <sorento6@ya.ru>
2020-12-27 19:18:43 +0300

    uptade code

--- krestiki-noliki.py
+++ krestiki-noliki.py
@@ -31,19 +31,24 @@ while icount <= 9:
         while True:
             column = int(input('Пользователь ' + str(player_id) + ' - введите номер столбца от 1 до 3: '))
             row = int(input('Пользователь ' + str(player_id) + ' - введите номер строки от 1 до 3: '))
+            #os.system('cls' if os.name == 'nt' else 'clear')
             
             
             if column < 1 or column > 3 or row < 1 or row > 3:
                 print('Пожалуйста введите номер от 1 до 3')
+                #print(board)
                 
             else:
                 break
         
         if board[row-1][column-1] == ' ':
             board[row-1][column-1] = player_move
+            os.system('cls' if os.name == 'nt' else 'clear')
             break
         else:
             print('Ячейка занята, выберите другую ячейку :')
+            #print(board)
+            
 
     print(''+board[0][0]+'|'+board[0][1]+'|'+board[0][2])
     print('-----')
@@ -56,7 +61,7 @@ while icount <= 9:
            board[0][ii]==board[1][ii]==board[2][ii]==player_move or \
            board[0][0]==board[1][1]==board[2][2]==player_move or \
            board[0][2]==board[1][1]==board[2][0]==player_move:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            #os.system('cls' if os.name == 'nt' else 'clear')
             print('Пользователь ' + str(player_id) + ' выиграл!')
             icount = 100
             break
@@ -65,5 +70,6 @@ while icount <= 9:
 
 if icount==10:
     print("Это ничья!")
+    
 
 
